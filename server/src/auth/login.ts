495d6cef0931eadb44d6e1fbf7f9c7ecbd33b0d7
code-pankaj <pankajyadavpy069@gmail.com>
2025-09-07 13:09:52 +0530

    login route redefined

--- server/src/auth/login.ts
+++ server/src/auth/login.ts
@@ -1,4 +1,8 @@
 import { type Request, type Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
 
 type credentials = {
     email: string,
@@ -13,6 +17,20 @@ export const login = async (req: Request, res: Response) => {
     }
 
     try {
+        const userExist = await prisma.user.findUnique({
+            where: {
+                email: email,
+            }
+        })
+        if(!userExist){
+            return res.status(400).send(`Email does not exist.`)
+        }
+        const passwordMatch = await bcrypt.compare(password, userExist.passwordHash)
+
+        if(!passwordMatch){
+            return res.status(401).send(`Wrong Password`)
+        }
+        return res.status(200).send(`Welcome back, ${userExist.username}`)
         
     } catch (error) {
         res.status(400).send(`Error encountered : ${error}`)
